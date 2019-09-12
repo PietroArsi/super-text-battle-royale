756d6314535ca7de4c6e6b645186690ac2ed9ec4
Davide Merli <davide.merli@outlook.com>
2019-09-12 19:30:49 +0200

    Annotazione setting, Tiles

---
+++ src/org/supertextbattleroyale/game/GameInstance.java
@@ -0,0 +1,16 @@
+package org.supertextbattleroyale.game;
+
+import org.supertextbattleroyale.maps.Map;
+import org.supertextbattleroyale.players.Player;
+
+import java.util.List;
+
+public class GameInstance {
+
+    private Map currentMap;
+
+    private List<Player> allPlayers, alivePlayers, deadPlayers;
+
+
+
+}
