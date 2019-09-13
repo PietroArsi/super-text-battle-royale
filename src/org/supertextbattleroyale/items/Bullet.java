3119671c75036406924ab6aeaaba08932d1c83eb
daryx77 <passarellodariox@gmail.com>
2019-09-13 17:26:35 +0200

    Poche cose sui Bullet

--- src/org/supertextbattleroyale/items/Bullet.java
+++ src/org/supertextbattleroyale/items/Bullet.java
@@ -6,22 +6,33 @@ import java.awt.*;
 
 public class Bullet implements Drawable {
 
+    private final static float BULLET_SPEED = 0.3f;
+    private final static float DISTANCE_BEFORE_DYING = 1.0f;
+
     private float x;
     private float y;
 
-    private float xDest;
-    private float yDest;
+    private float xTarget;
+    private float yTarget;
 
     private boolean miss;
 
     public Bullet(float x, float y, float xDest, float yDest, boolean miss) {
         this.x = x;
         this.y = y;
-        this.xDest = xDest;
-        this.yDest = yDest;
+        this.xTarget = xDest;
+        this.yTarget = yDest;
         this.miss = miss;
     }
 
+    private void moveBullet() {
+        float distance = distanceFromTarget();
+        //TODO
+    }
+
+    private float distanceFromTarget(){
+        return (float) Math.sqrt(Math.pow(x - xTarget,2) + Math.pow(y - yTarget,2));
+    }
 
     @Override
     public void draw(Graphics2D g) {
