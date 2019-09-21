c4c623fc50aa6d8785849f4c0773136b130ddf17
Davide <davide.merli@outlook.com>
2019-09-22 01:03:59 +0200

    Movement status

--- src/org/supertextbattleroyale/players/statuses/Combat.java
+++ src/org/supertextbattleroyale/players/statuses/Combat.java
@@ -21,31 +21,31 @@ public class Combat extends Status {
 
     @Override
     public Status doStatusAction() {
-        if(this.player.getPlayersSeen().isEmpty()) return new Movement(this.player, this.player.getCurrentMap().getMapCenter());
+        if (this.player.getPlayersSeen().isEmpty())
+            return new Movement(this.player, this.player.getCurrentMap().getMapCenter());
 
-        if(RandomUtils.bernoulli( 1 - this.player.getHitPoints() / (2f * this.player.getMaxHitPoints())) == 1) {
+        if (RandomUtils.bernoulli(1 - this.player.getHitPoints() / (2f * this.player.getMaxHitPoints())) == 1) {
             //Get all visible doors
             List<Point> doors = player.getKnownPlaces().stream()
                     .filter(pair -> pair.getValue0() instanceof Door)
                     .map(Pair::getValue1).collect(Collectors.toList());
 
             int[][] distances = MapUtils.calculateDistances(player.getCurrentMap(), (aMap, aPoint) -> player.getCurrentMap()
-                    .getPlayersOnMap().stream()
-                    .map(p -> p.getPoint())
-                    .collect(Collectors.toList())
-                    .stream()
-                    .noneMatch(p -> p.equals(aPoint)) && aMap.getMatrixMap()[aPoint.x][aPoint.y].isTileWalkable(),
+                            .getPlayersOnMap().stream()
+                            .map(p -> p.getPoint())
+                            .collect(Collectors.toList())
+                            .stream()
+                            .noneMatch(p -> p.equals(aPoint)) && aMap.getMatrixMap()[aPoint.x][aPoint.y].isTileWalkable(),
                     doors, false);
 
-            for(int i = 0; i < FLEE_DISTANCE; i++) {
-                MapUtils.getNextPathStep(player.getCurrentMap(),distances,player.getPoint(),false);
+            for (int i = 0; i < FLEE_DISTANCE; i++) {
+                MapUtils.getNextPathStep(player.getCurrentMap(), distances, player.getPoint(), false);
                 //TODO: Controllare l'optional
             }
 
 
-
         }
 
-
+        return null;
     }
 }
